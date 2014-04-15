11ae3a45bb7a80b4ad55604f6bf2f1eeee13918c
Tayfun Erbilen <tayfunerbilen@gmail.com>
2014-04-15 12:03:57 +0300

    Update insert_example.php

--- insert_example.php
+++ insert_example.php
@@ -9,9 +9,9 @@ $db = new BasicDB('localhost', 'testdb', 'testuser', 'password');
 // insert
 $query = $db->insert('users')
             ->set(array(
-                        username => 'test user',
-              password => 123456,
-              email => 'test@mail.com'
+                 username => 'test user',
+                 password => 123456,
+                 email => 'test@mail.com'
             ));
    
 if ( $query ){
