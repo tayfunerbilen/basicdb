3bb4a83f8cfe7aae5fa981fe8fe404f5423cc4f5
Tayfun Erbilen <tayfunerbilen@gmail.com>
2017-04-22 01:21:11 +0300

    yeni versiyona göre güncellendi.

--- examples/update_example.php
+++ examples/update_example.php
@@ -9,9 +9,9 @@ $db = new BasicDB('localhost', 'testdb', 'testuser', 'password');
 // update
 $query = $db->update('users')
             ->where('user_id', 2)
-            ->set(array(
-                 username => 'another user'
-            ));
+            ->set([
+                 'username' => 'another user'
+            ]);
    
 if ( $query ){
   echo 'update success';
